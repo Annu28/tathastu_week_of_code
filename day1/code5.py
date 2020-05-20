2d5c24d5edec4741188b16a4b8eaf117f45638f1
Annu28 <65583989+Annu28@users.noreply.github.com>
2020-05-20 12:55:04 +0530

    Create code5.py

---
+++ day1/code5.py
@@ -0,0 +1,15 @@
+score1=int(input("enter score of player 1"))
+score2=int(input("enter score of player 2"))
+score3=int(input("enter score of player 3"))
+sr1=score1*100 / 60
+sr2=score2*100 / 60
+sr3=score3*100 / 60
+print("strike rate of player 1 is:",sr1)
+print("strike rate of player 2 is:",sr2)
+print("strike rate of player 3 is:",sr3)
+print("score when player 1 played 60 balls more",score1*2)
+print("score when player 2 played 60 balls more",score2*2)
+print("score when player 3 played 60 balls more",score3*2)
+print("max. six player 1 achieved",score1//6)
+print("max. six player 2 achieved",score2//6)
+print("max. six player 3 achieved",score3//6)
