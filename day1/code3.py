07810462dac746e1f0eca9393fdf3f26f75e560a
Annu28 <65583989+Annu28@users.noreply.github.com>
2020-05-20 12:37:42 +0530

    Create code3.py

---
+++ day1/code3.py
@@ -0,0 +1,8 @@
+n1=int(input("enter first number"))
+n2=int(input("enter second number"))
+n1=n1+n2
+n2=n1-n2
+n1=n1-n2
+print("after swipping")
+print("first number ",n1)
+print("second number ",n2)
