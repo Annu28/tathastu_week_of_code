a3c3bf5fc3ee15ef901404ed6039183eea2b03a0
Annu28 <65583989+Annu28@users.noreply.github.com>
2020-05-20 12:42:43 +0530

    Create code4.py

---
+++ day1/code4.py
@@ -0,0 +1,6 @@
+cp=float(input("enter cost price"))
+sp=float(input("enter selling price"))
+profit=sp-cp
+print("profit: ",profit)
+new_sp=(105/100)*profit + cp
+print("after adding five percent selling price is:",new_sp)
