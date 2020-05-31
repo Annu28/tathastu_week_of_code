9daa6fc8f78b53f4e59cfe409e56ac01e579d17a
Annu28 <65583989+Annu28@users.noreply.github.com>
2020-06-01 00:59:05 +0530

    Create code9.py

---
+++ day6/code9.py
@@ -0,0 +1,10 @@
+  
+def small(arr, n, k): 
+    arr.sort() 
+    return arr[k-1] 
+if __name__=='__main__': 
+    arr = [12, 3, 5, 7, 19] 
+    n = len(arr) 
+    k = 2
+    print("K'th smallest element is", 
+          small(arr, n, k))
